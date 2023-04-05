cb78e99bcdd9b6b79d3a8e33f26a638636305fe9
hata-junnosuke <jh19911209@icloud.com>
2023-04-05 21:24:39 +0900

    [fix]rubocop  -a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-    # フラッシュの種類
-    add_flash_types :primary, :success, :warning, :danger
+  # フラッシュの種類
+  add_flash_types :primary, :success, :warning, :danger
 end
