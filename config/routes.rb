c94c16b2f76b8cc1ad93b26e1296f08503d49495
hata-junnosuke <jh19911209@icloud.com>
2023-04-05 21:03:00 +0900

    [add]ユーザー登録機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/health_check', to: 'health_checks#show'
-  
+
   root to: 'samples#index'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 end
