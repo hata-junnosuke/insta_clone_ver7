4670cf980fbac32e73871e311bc89a3629d2d34d
hata-junnosuke <jh19911209@icloud.com>
2023-04-06 22:14:50 +0900

    [add]投稿機能追加(テストは失敗)

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get '/login', to: 'user_sessions#new'
   post '/login', to: 'user_sessions#create'
   delete '/logout', to: 'user_sessions#destroy'
+
+  resources :posts
 end
