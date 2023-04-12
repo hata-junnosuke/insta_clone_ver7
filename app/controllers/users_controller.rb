2b395c5ec142f2a93885303aedcdf0954080614f
hata-junnosuke <jh19911209@icloud.com>
2023-04-12 20:49:30 +0900

    [fix]rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @pagy, @users = pagy(User.order(created_at: :desc))
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -15,10 +19,6 @@ class UsersController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
-  end
-
-  def show
-    @user = User.find(params[:id])
   end
 
   private
