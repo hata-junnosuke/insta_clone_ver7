0885bf7eb7fcaf610e18b4e2413769498d3a37f1
hata-junnosuke <jh19911209@icloud.com>
2023-04-12 11:27:28 +0900

    [add]検索機能

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -24,4 +24,8 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates :body, presence: true, length: { maximum: 1000 }
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["body", "created_at", "id", "post_id", "updated_at", "user_id"]
+  end
 end
